97c40b457381b347c0fb739222d7392280be3e91
BetsM <bez.pmw@gmail.com>
2022-07-18 13:29:52 +0300

    Group score by house; icons

--- score/script.js
+++ score/script.js
@@ -3,38 +3,65 @@ const sourceSheetId = "1hUllfU7bqfNBu42vl7M0tzm2In5YZ5TqdqIH7p2pBuQ";
 // GET GOOGLE SHEET DATA
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(function () {
-     var query = new google.visualization.Query(
-          `https://docs.google.com/spreadsheets/d/${sourceSheetId}/gviz/tq`
-     );
+    var query = new google.visualization.Query(
+        `https://docs.google.com/spreadsheets/d/${sourceSheetId}/gviz/tq`
+    );
 
-     query.setQuery("select A,B");
-     //className,points
-     query.setRefreshInterval(20);
-     query.send(handleQueryResponse);
+    query.setQuery("select A,B");
+    //className,points
+    query.setRefreshInterval(20);
+    query.send(handleQueryResponse);
 });
 var data;
+var oldHouses;
 function handleQueryResponse(re) {
-     if (re.isError()) {
-          console.log(
-               `Error in query: ${re.getMessage()} ${re.getDetailedMessage()}`
-          );
-          return;
-     }
-     data = re.getDataTable();
-     //CREATE
-     var buildingBlocks = getCol(0, 1);
-     buildingBlocks = buildingBlocks.sort(function (a, b) {
-          return b.val - a.val;
-     });
-     // cl(buildingBlocks.length);
-     qi("dynamic").replaceChildren();
-     for (const item of buildingBlocks) {
-          // cl(item);
-          let node = createHtml(
-               `<p><label>${item.key}</label><span>${item.val}</span></p>`
-          );
-          qi("dynamic").append(node);
-     }
+    if (re.isError()) {
+        console.log(
+            `Error in query: ${re.getMessage()} ${re.getDetailedMessage()}`
+        );
+        return;
+    }
+    data = re.getDataTable();
+    //CREATE
+    var classScores = getCol(0, 1);
+    qi("dynamic").replaceChildren();
+
+    var houses = new Map([
+        ["aria", 0],
+        ["batsir", 0],
+        ["gadid", 0]
+    ]);
+    let oldScore;
+    for (const clas of classScores) {
+        switch (true) {
+            case (clas.key.includes("ארייה")):
+                oldScore = houses.get('aria');
+                houses.set('aria', oldScore += clas.val);
+                break;
+            case (clas.key.includes("בציר")):
+                oldScore = houses.get('batsir');
+                houses.set('batsir', oldScore += clas.val);
+                break;
+            case (clas.key.includes("גדיד")):
+                oldScore = houses.get('gadid');
+                houses.set('gadid', oldScore += clas.val);
+                break;
+            default: cl("found nothing");
+        }
+    }
+    //cl(houses);
+    houses = new Map([...houses.entries()].sort(function (a, b) {
+        return b[1] - a[1];
+    }));
+    //cl(houses);
+
+    for (const [name, score] of houses) {
+        let node = createHtml(
+            `<div class="scoreRow${oldHouses && oldHouses.get(name) != score ?' bgTrans':''}"><img width="100" src="../img/${name}.png"/><span>${score}</span></div>`//<label>${item.key}</label>
+        );
+        qi("dynamic").append(node);
+    }
+    oldHouses = houses;
 }
 
 // HANDEL DATA FROM SHEETS
@@ -43,25 +70,25 @@ function handleQueryResponse(re) {
 //return as array of {lable,val}
 //If only 1 column so val will be index number
 function getCol(col, valCol) {
-     var rows = [];
-     var key;
-     for (let i = 0; i < data.getNumberOfRows(); i++) {
-          key = getValue(i, col);
-          let cell = isNaN(key) ? key?.trim() : key.toString();
-          if (cell.length == 0) break;
-          rows.push({
-               key: key,
-               val: valCol ? getValue(i, valCol) : i + 1
-          });
-     }
-     return rows;
+    var rows = [];
+    var key;
+    for (let i = 0; i < data.getNumberOfRows(); i++) {
+        key = getValue(i, col);
+        let cell = isNaN(key) ? key?.trim() : key.toString();
+        if (cell.length == 0) break;
+        rows.push({
+            key: key,
+            val: valCol ? getValue(i, valCol) : i + 1
+        });
+    }
+    return rows;
 }
 
 //get value from sheet db (starts at 0)
 function getValue(row, col) {
-     // if (row + 2 == data.getNumberOfRows()) return "";
-     // return data.getValue(row + 2, col) ?? "";
-     return data.getValue(row, col);
+    // if (row + 2 == data.getNumberOfRows()) return "";
+    // return data.getValue(row + 2, col) ?? "";
+    return data.getValue(row, col);
 }
 
 //Get the data-val from the option of the chosen key
@@ -76,7 +103,7 @@ function getValue(row, col) {
 // CREATE ELEMENTS
 
 function createHtml(str) {
-     return document.createRange().createContextualFragment(str);
+    return document.createRange().createContextualFragment(str);
 }
 // function getNextLetter(char) {
 //      return String.fromCharCode(char.charCodeAt(char.length - 1) + 1); //go to next letter
@@ -132,12 +159,12 @@ function createHtml(str) {
 // }
 
 function saveToStorage() {
-     localStorage.setItem("name", qs("#name input").value);
+    localStorage.setItem("name", qs("#name input").value);
 }
 function restoreFromStorage() {
-     let saved = localStorage.getItem("name");
-     if (saved == null) return;
-     qs("#name input").value = saved;
+    let saved = localStorage.getItem("name");
+    if (saved == null) return;
+    qs("#name input").value = saved;
 }
 
 // CALCULATIONS
@@ -291,23 +318,23 @@ function restoreFromStorage() {
 
 // SELECTORS
 function qs(s) {
-     return document.querySelector(s);
+    return document.querySelector(s);
 }
 function qsa(s) {
-     return document.querySelectorAll(s);
+    return document.querySelectorAll(s);
 }
 function qi(s) {
-     return document.getElementById(s);
+    return document.getElementById(s);
 }
 function qc(s) {
-     return document.getElementsByClassName(s)[0];
+    return document.getElementsByClassName(s)[0];
 }
 function qca(s) {
-     return document.getElementsByClassName(s);
+    return document.getElementsByClassName(s);
 }
 function qp(p, n) {
-     return document.querySelector(`[${p}="${n}"]`);
+    return document.querySelector(`[${p}="${n}"]`);
 }
 function cl(txt) {
-     console.log(txt);
+    console.log(txt);
 }
